954f9ae18013707e8fe57d520a3f6fe6adf3af41
Alejandrolt <alejandrolt54@gmail.com>
2024-06-25 21:38:53 -0500

    nodejs4

--- models/contacts.js
+++ models/contacts.js
@@ -1,22 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const contactsSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Set name for contact'],
-    },
-    email: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-  }
-)
+const contactsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
 
-module.exports= mongoose.model("contacts",contactsSchema)
+module.exports = mongoose.model("contacts", contactsSchema);
